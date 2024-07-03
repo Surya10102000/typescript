6fdd2913ab343a22f5b71b7bc095bf534e967371
Surya Prakash Mallah <sfrost488@gmail.com>
2024-07-03 22:43:03 +0530

    intial commit

---
+++ basics/calSum.js
@@ -0,0 +1,8 @@
+function calculateSum(a, b) {
+    return a + b;
+}
+console.log(calculateSum(2, 3));
+// function jscalculateSum (a 
+//      , b){
+//     return a+b
+// }
---
+++ basics/calSum.ts
@@ -0,0 +1,10 @@
+function calculateSum (a : number, b : number ) : number {
+    return a + b;
+}
+
+console.log(calculateSum(2,3));
+
+// function jscalculateSum (a 
+//      , b){
+//     return a+b
+// }
